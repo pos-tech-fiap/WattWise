f62c8a68a80c2e2d02277997118ca99a5f4837c3
Bruno Miguel Morais <bm.morais21@gmail.com>
2023-06-21 20:30:28 -0300

    Criado model de pessoa e enum de parentesco

---
+++ src/main/java/com/pos/wattwise/models/person/Person.java
@@ -0,0 +1,88 @@
+package com.pos.wattwise.models.person;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Person {
+
+    @NotNull
+    private UUID id;
+
+    @NotNull
+    @NotEmpty
+    private String name;
+
+    private String gender;
+
+    private Date birthDate;
+
+    @Email
+    private String email;
+
+    private Number phone;
+
+    private Kinship kinship;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Number getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Number phone) {
+        this.phone = phone;
+    }
+
+    public Kinship getKinship() {
+        return kinship;
+    }
+
+    public void setKinship(Kinship kinship) {
+        this.kinship = kinship;
+    }
+
+    public Person() {
+    }
+}
