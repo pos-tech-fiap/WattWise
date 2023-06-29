f0e2c0834c564c6909463c45d71c680a751400df
eugabiviana <gabi.viana88@gmail.com>
2023-06-29 19:40:27 -0300

    Correcting text on DTO.

--- src/main/java/com/pos/wattwise/dtos/ElectrodomesticsDTO.java
+++ src/main/java/com/pos/wattwise/dtos/ElectrodomesticsDTO.java
@@ -4,6 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 
 public record ElectrodomesticsDTO( @JsonProperty  @NotBlank(message = "Name can not be blank!") String name,
-                                   @JsonProperty @NotBlank(message = "Name can not be blank!") String type,
-                                   @JsonProperty @NotBlank(message = "Name can not be blank!") String power) {
+                                   @JsonProperty @NotBlank(message = "Type can not be blank!") String type,
+                                   @JsonProperty @NotBlank(message = "Power can not be blank!") String power) {
 }
