a02610d5f0b4b9f75015e2e9acbc592e7c82ed8c
Thcamposmartins <thcampos_@live.com>
2023-08-27 14:30:40 -0300

    Adicionando sql completo

--- init.sql
+++ init.sql
@@ -4,26 +4,47 @@
 -- Connect to the wattwise database
 \c wattwise;
 
--- Create the tb_electronic table
-CREATE TABLE IF NOT EXISTS tb_electronics (
-    id UUID PRIMARY KEY,
-    name VARCHAR(255),
-    model VARCHAR(100),
-    power VARCHAR(50)
+CREATE TABLE IF NOT EXISTS tb_person(
+    id UUID primary key,
+    name VARCHAR(255) not null,
+    gender VARCHAR(255),
+    birthDate date not null,
+    email VARCHAR(255),
+    phone VARCHAR(15)
 );
 
--- Create the tb_address table
-CREATE TABLE IF NOT EXISTS tb_address (
-    id SERIAL PRIMARY KEY,
-    street VARCHAR(100),
-    city VARCHAR(50),
-    country VARCHAR(50)
+CREATE TABLE IF NOT EXISTS tb_address(
+    id UUID primary key,
+    street VARCHAR(255) not null,
+    number numeric not null,
+    complement VARCHAR(255),
+    neighborhood VARCHAR(255),
+    city VARCHAR(255) not null,
+    state VARCHAR(255) not null,
+    zip_code VARCHAR(15) not null
 );
 
--- Create the tb_person table
-CREATE TABLE IF NOT EXISTS tb_person (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    age INT
+CREATE TABLE IF NOT EXISTS tb_electronic(
+    id UUID primary key,
+    name VARCHAR(255) not null,
+    model VARCHAR(255) not null,
+    power VARCHAR(255) not null,
+    address_id UUID not null,
+    foreign key(address_id) references tb_address(id) on delete cascade;
+);
+
+CREATE TABLE IF NOT EXISTS tb_person_address(
+    person_id UUID not null,
+    address_id UUID not null,
+    primary key(person_id, address_id),
+    foreign key(person_id) references tb_person(id) on delete cascade,
+    foreign key(address_id) references tb_address(id) on delete cascade
+);
+
+create table IF NOT EXISTS tb_person_electronic(
+    person_id UUID not null,
+    electronic_id UUID not null,
+    primary key(person_id, electronic_id),
+    foreign key(person_id) references tb_person(id) on delete cascade,
+    foreign key(electronic_id) references tb_electronic(id) on delete cascade
 );
